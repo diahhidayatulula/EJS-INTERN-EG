3eca08b955a4cd6e97655b342611afc1343115ac
Diah Hidayatul Ula <217006003@student.unsil.ac.id>
2024-07-09 15:45:48 +0700

    feat : profile

---
+++ src/controllers/user.js
@@ -0,0 +1,15 @@
+export const profile = (req, res) => {
+    res.render("pages/user/profile.ejs");
+};
+
+export const affiliate = (req, res) => {
+    res.render("pages/user/affiliate.ejs");
+};
+
+export const myProject = (req, res) => {
+    res.render("pages/user/myproject.ejs");
+};
+
+export const withDraw = (req, res) => {
+    res.render("pages/user/withdraw.ejs");
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
+import {home} from "../controllers/pages.js"
+import { cekTurnitin, ParafraseIn, sarangFreelance, translateIn } from "../controllers/services.js";
+import { affiliate, myProject, profile, withDraw } from "../controllers/user.js";
+
 export const route = function (app) {
-    app.get("/", (req,res) => {
-        res.render ("pages/home")
-    });
-    
-    app.get("/home", (resq,res) => {
-        res.render("pages/home")
-    });
-    
-    app.get("/cek-turnitin", (resq,res) => {
-        res.render("pages/cek-turnitin.ejs/index.ejs")
-    });
+    app.get("/", home);
+    app.get("/cek-turnitin", cekTurnitin);
+    app.get("/parafrase-in", ParafraseIn);
+    app.get("/translate-in", translateIn);
+    app.get("/sarang-freelance", sarangFreelance);
+    app.get("/profile", profile);
+    app.get("/affiliate", affiliate);
+    app.get("/myproject", myProject);
+    app.get("/withdraw", withDraw);
 }
